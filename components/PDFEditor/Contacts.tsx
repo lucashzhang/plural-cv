b552f3121fb492c299b3e6db45167991d367d500
Lucas Zhang <lhzhang@live.unc.edu>
2022-07-04 12:06:03 -0700

    Moved out of website folder

--- components/PDFEditor/Contacts.tsx
+++ components/PDFEditor/Contacts.tsx
@@ -6,7 +6,7 @@ const PDFEditor: FC = () => {
 
     const { dispatch, contacts } = useContext(PDFContext);
 
-    function handleContactChange(e: ChangeEvent<HTMLInputElement>, key: string) {
+    function handleChange(e: ChangeEvent<HTMLInputElement>, key: string) {
         dispatch({
             type: pdfDataActions.MODIFY_CONTACTS,
             payload: {
@@ -16,14 +16,14 @@ const PDFEditor: FC = () => {
         })
     }
 
-    function handleContactDelete(key: string) {
+    function handleDelete(key: string) {
         dispatch({
             type: pdfDataActions.REMOVE_CONTACTS,
             payload: key
         })
     }
 
-    function handleContactAdd() {
+    function handleAdd() {
         dispatch({
             type: pdfDataActions.ADD_CONTACTS,
             payload: {
@@ -40,15 +40,15 @@ const PDFEditor: FC = () => {
                 {contacts.map(({ item, key }) => (
                     <div className="form-control py-1" key={key}>
                         <div className="input-group w-full">
-                            <input type="text" placeholder="Input Contact" className="input input-bordered w-full" value={item} onChange={(e) => handleContactChange(e, key)} />
-                            <button className="btn btn-square" onClick={() => handleContactDelete(key)}>
+                            <input type="text" placeholder="Input Contact" className="input input-bordered w-full" value={item} onChange={(e) => handleChange(e, key)} />
+                            <button className="btn btn-square" onClick={() => handleDelete(key)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                             </button>
                         </div>
                     </div>
                 ))}
             </div>
-            <button className='btn' onClick={handleContactAdd}>Add New Contact</button>
+            <button className='btn' onClick={handleAdd}>Add New Contact</button>
         </div>
     )
 }
--- util/PDFDoc.tsx
+++ util/PDFDoc.tsx
@@ -10,12 +10,12 @@ const PDFDoc: FC<PDFProps> = ({ data, styles }) => {
         <Document>
             <Page size="A4" style={styles.page}>
                 <View style={styles.header}>
+                    <Text>Lucas Zhang</Text>
+                </View>
+                <View style={styles.header}>
                     {contacts.map((contact) => (
                         <Text key={contact.key}>{contact.item}</Text>
                     ))}
-                </View>
-                <View style={styles.header}>
-                    <Text>Section #2</Text>
                 </View>
             </Page>
         </Document>
