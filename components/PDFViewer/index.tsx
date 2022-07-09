c36bfe42f3ad19a14c6f6e509a96085972537b9e
Lucas Zhang <lhzhang@live.unc.edu>
2022-07-09 12:05:34 -0700

    Removing it all

--- components/PDFViewer/index.tsx
+++ components/PDFViewer/index.tsx
@@ -1,10 +1,10 @@
 import { FC, useRef, useContext, useEffect } from "react";
 // import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
-import { StyleSheet, usePDF } from "@react-pdf/renderer";
-import { PDFContext } from "../../util/PDFProvider";
-import PDFDoc from "../../util/PDFDoc";
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import { PDFStyle } from "../../util/PDFTypes";
+// import { StyleSheet, usePDF } from "@react-pdf/renderer";
+// import { PDFContext } from "../../util/PDFProvider";
+// import PDFDoc from "../../util/PDFDoc";
+// import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+// import { PDFStyle } from "../../util/PDFTypes";
 
 // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -16,43 +16,43 @@ const Viewer: FC<Prop> = ({ pageNumber }) => {
 
     const puppetCanvas: any = useRef();
     const shadowCanvas: any = useRef();
-    const { template, contacts, education, work, activities, skills, awards } = useContext(PDFContext);
-    const pdfState = {
-        template,
-        contacts,
-        education,
-        work,
-        activities,
-        skills,
-        awards
-    }
-    const initialStyle: PDFStyle = StyleSheet.create({
-        page: {},
-        header: {}
-    })
-    const doc = <PDFDoc data={pdfState} styles={initialStyle} />
+    // const { template, contacts, education, work, activities, skills, awards } = useContext(PDFContext);
+    // const pdfState = {
+    //     template,
+    //     contacts,
+    //     education,
+    //     work,
+    //     activities,
+    //     skills,
+    //     awards
+    // }
+    // const initialStyle: PDFStyle = StyleSheet.create({
+    //     page: {},
+    //     header: {}
+    // })
+    // const doc = <PDFDoc data={pdfState} styles={initialStyle} />
 
-    const [instance, setInstance] = usePDF({ document: doc })
+    // const [instance, setInstance] = usePDF({ document: doc })
 
-    function cleanViewPort() {
-        const textLayers = document.querySelectorAll<HTMLElement>(".react-pdf__Page__textContent");
-        textLayers.forEach(layer => {
-            const { style } = layer;
-            style.top = "0";
-            style.left = "0";
-            style.transform = "";
-        });
-    }
+    // function cleanViewPort() {
+    //     const textLayers = document.querySelectorAll<HTMLElement>(".react-pdf__Page__textContent");
+    //     textLayers.forEach(layer => {
+    //         const { style } = layer;
+    //         style.top = "0";
+    //         style.left = "0";
+    //         style.transform = "";
+    //     });
+    // }
 
-    function renderPuppet() {
-        puppetCanvas.current.width = shadowCanvas.current.width;
-        puppetCanvas.current.height = shadowCanvas.current.height;
-        puppetCanvas.current.style.width = shadowCanvas.current.style.width;
-        puppetCanvas.current.style.height = shadowCanvas.current.style.height;
-        puppetCanvas.current.getContext('2d').drawImage(shadowCanvas.current, 0, 0);
-    }
+    // function renderPuppet() {
+    //     puppetCanvas.current.width = shadowCanvas.current.width;
+    //     puppetCanvas.current.height = shadowCanvas.current.height;
+    //     puppetCanvas.current.style.width = shadowCanvas.current.style.width;
+    //     puppetCanvas.current.style.height = shadowCanvas.current.style.height;
+    //     puppetCanvas.current.getContext('2d').drawImage(shadowCanvas.current, 0, 0);
+    // }
 
-    useEffect(setInstance, [doc]);
+    // useEffect(setInstance, [doc]);
 
     return (
         <div className="rounded overflow-hidden relative">
