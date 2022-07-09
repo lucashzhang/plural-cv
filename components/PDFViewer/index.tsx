63794f7e9ad5a9774eac95eea05aaf1474d917a5
Lucas Zhang <lhzhang@live.unc.edu>
2022-07-09 13:31:16 -0700

    Stuffing inside of useEffect

--- components/PDFViewer/index.tsx
+++ components/PDFViewer/index.tsx
@@ -1,10 +1,10 @@
 import { FC, useRef, useContext, useEffect } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
-// import { StyleSheet } from "@react-pdf/renderer";
+import { StyleSheet } from "@react-pdf/renderer";
 import { PDFContext } from "../../util/PDFProvider";
-// import PDFDoc from "../../util/PDFDoc";
+import PDFDoc from "../../util/PDFDoc";
 // import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-// import { PDFStyle } from "../../util/PDFTypes";
+import { PDFStyle } from "../../util/PDFTypes";
 
 // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -17,20 +17,25 @@ const Viewer: FC<Prop> = ({ pageNumber }) => {
     const puppetCanvas: any = useRef();
     const shadowCanvas: any = useRef();
     const { template, contacts, education, work, activities, skills, awards } = useContext(PDFContext);
-    const pdfState = {
-        template,
-        contacts,
-        education,
-        work,
-        activities,
-        skills,
-        awards
-    }
-    // const initialStyle: PDFStyle = StyleSheet.create({
-    //     page: {},
-    //     header: {}
-    // })
-    // const doc = <PDFDoc data={pdfState} styles={initialStyle} />
+
+
+    useEffect(() => {
+        const initialStyle: PDFStyle = StyleSheet.create({
+            page: {},
+            header: {}
+        })
+        const pdfState = {
+            template,
+            contacts,
+            education,
+            work,
+            activities,
+            skills,
+            awards
+        }
+        const doc = <PDFDoc data={pdfState} styles={initialStyle} />
+    }, [template, contacts, education, work, activities, skills, awards])
+
 
     // const [instance, setInstance] = usePDF({ document: doc })
 
